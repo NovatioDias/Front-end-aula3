d4a965237ca8bd85f6b36ca9a0cb831b0912e2cf
NovatioDias <86272180+NovatioDias@users.noreply.github.com>
2022-03-22 00:28:20 -0300

    Create script3_01.js

---
+++ script3_01.js
@@ -0,0 +1,12 @@
+var dia, nota;//declarar as var
+dia = 25;//var dia recebe o numero inteiro 25
+nota = 9.7;//var nota recebe o numero float(real) 9.7
+document.write (dia);
+document.write ("<br>");//quebra linha
+document.write (nota);
+/*
+saida:
+25
+9.7
+
+*/
